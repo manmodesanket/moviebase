4e903079741846fb7faf3404a1c4307f04a2041e
Sanket Manmode <manmodesanket@gmail.com>
2023-12-13 19:12:33 +0530

    fix: Skeleton style change

--- src/app/discover/moviesSkeleton.tsx
+++ src/app/discover/moviesSkeleton.tsx
@@ -1,13 +1,15 @@
 const ImageSkeleton = () => {
   return (
-    <div className="animate-pulse rounded-md overflow-hidden bg-gray-300 p-2">
-      <div className="lg:h-80 h-56 w-52"></div> {/* Adjust height as needed */}
+    <div className="overflow-hidden p-2">
+      <div
+        style={{ height: "315px", width: "210px" }}
+        className="lg:h-80 bg-gray-300 rounded-md animate-pulse"
+      ></div>{" "}
     </div>
   );
 };
 
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
   return (
     <div className="grid grid-cols-2 lg:grid-cols-4 gap-2 mt-8">
       <ImageSkeleton />
