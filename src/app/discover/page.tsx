39cc40225179f9b251f3bf999cdf4e0b785844e8
Sanket Manmode <manmodesanket@gmail.com>
2024-01-12 17:46:27 +0530

    feat: Tabs added on discover route

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,12 +1,30 @@
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/shadcn-ui/tabs";
 import Search from "./search";
 
 export default function Discover() {
   return (
     <div id="page-content" className="flex flex-col">
-      <h2 className="text-2xl p-2 lg:p-0 font-semibold tracking-tight">
-        Discover
-      </h2>
-      <Search />
+      <Tabs defaultValue="name" className="">
+        <TabsList className="bg-muted w-[300px] mx-auto">
+          <TabsTrigger className="w-[150px]" value="name">
+            Name
+          </TabsTrigger>
+          <TabsTrigger className="w-[150px]" value="filters">
+            Filters
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="name">
+          <Search />
+        </TabsContent>
+        <TabsContent value="filters">
+          <p className="p-2">Discover movies using filters</p>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
