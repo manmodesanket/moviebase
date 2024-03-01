364cfdcd00b45cb099ede9ab59baa0e88ad5f044
Sanket Manmode <manmodesanket@gmail.com>
2024-03-01 22:05:04 +0530

    refactor: Movie page loading style change

--- src/app/movie/[id]/loading.tsx
+++ src/app/movie/[id]/loading.tsx
@@ -1,23 +1,24 @@
+import { Skeleton } from "@/components/shadcn-ui/skeleton";
+
 export default async function Page({}) {
   return (
     <main className="p-2">
       <section className="lg:grid lg:grid-cols-2 mb-4">
-        <div className="animate-pulse">
-          <div className="rounded-md bg-slate-400 h-[315px] w-[210px] mx-auto lg:mx-0"></div>
-        </div>
-        <div className="mt-4 animate-pulse">
-          <div className="h-4 bg-slate-400 rounded col-span-2"></div>
+        <Skeleton className="h-[315px] w-[205px]" />
+        <div className="mt-4">
+          <Skeleton className="h-4 w-[250px]" />
           <div className="mt-4 flex gap-4">
-            <div className="h-4 w-[40px] bg-slate-400 rounded col-span-2"></div>
-            <div className="h-4 w-[40px] bg-slate-400 rounded col-span-2"></div>
+            <Skeleton className="h-4 w-[40px] rounded col-span-2" />
+            <Skeleton className="h-4 w-[40px] rounded col-span-2" />
           </div>
           <div className="mt-4 flex gap-4">
-            <div className="h-4 w-[80px] bg-slate-400 rounded col-span-2"></div>
+            <Skeleton className="h-6 w-[80px] rounded col-span-2" />
           </div>
           <div className="mt-4 flex flex-col gap-2">
-            <div className="h-2 bg-slate-400 rounded col-span-2"></div>
-            <div className="h-2 bg-slate-400 rounded col-span-2"></div>
-            <div className="h-2 bg-slate-400 rounded col-span-2"></div>
+            <Skeleton className="h-2 rounded col-span-2" />
+            <Skeleton className="h-2 rounded col-span-2" />
+            <Skeleton className="h-2 rounded col-span-2" />
+            <Skeleton className="h-2 rounded col-span-2" />
           </div>
         </div>
       </section>
